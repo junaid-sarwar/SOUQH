817785333ee6336763ce09d02fa041bd0b0909b7
Junaid Sarwar <junaidsarwariqbal@gmail.com>
2025-03-02 17:01:28 +0500

    working on navbar and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import './App.css';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Building SOUQH</h1>
+      {/* <h1 className='text-3xl font-bold underline'>Building SOUQH</h1> */}
+      <Navbar/>
+      <HeroSection/>
     </>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const HeroSection = () => {
-  return (
-    <div>Will Made by Junaid</div>
-  )
-}
+  const heroRef = useRef(null);
 
-export default HeroSection
+  useEffect(() => {
+    gsap.fromTo(
+      heroRef.current, 
+      { opacity: 0, y: 50 }, // Initial state
+      { opacity: 1, y: 0, duration: 1, ease: "power3.out" } // Final state
+    );
+  }, []);
+
+  return (
+    <section ref={heroRef} className="h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-5xl font-bold text-[#E6553A]">Hello, Welcome to SOUQH</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
